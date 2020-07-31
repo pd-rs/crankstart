25384e5ddfcc5b6de52934291243d9a16aec2079
Rob Tsuk <rob@tsuk.com>
2020-07-31 06:19:37 -0700

    Don’t use a unit struct with euclid
    Not clear the separation is worth the additional effort in
    conversion.

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,13 +1,11 @@
-pub struct ScreenSpace;
 pub type ScreenCoord = i32;
-pub type ScreenPoint = euclid::Point2D<ScreenCoord, ScreenSpace>;
-pub type ScreenVector = euclid::Vector2D<ScreenCoord, ScreenSpace>;
-pub type ScreenRect = euclid::Rect<ScreenCoord, ScreenSpace>;
-pub type ScreenSize = euclid::Size2D<ScreenCoord, ScreenSpace>;
+pub type ScreenPoint = euclid::default::Point2D<ScreenCoord>;
+pub type ScreenVector = euclid::default::Vector2D<ScreenCoord>;
+pub type ScreenRect = euclid::default::Rect<ScreenCoord>;
+pub type ScreenSize = euclid::default::Size2D<ScreenCoord>;
 
-pub struct GrSpace;
 pub type GrCoord = f32;
-pub type GrPoint = euclid::Point2D<GrCoord, GrSpace>;
-pub type GrVector = euclid::Vector2D<GrCoord, GrSpace>;
-pub type GrRect = euclid::Rect<GrCoord, GrSpace>;
-pub type GrSize = euclid::Size2D<GrCoord, GrSpace>;
+pub type GrPoint = euclid::default::Point2D<GrCoord>;
+pub type GrVector = euclid::default::Vector2D<GrCoord>;
+pub type GrRect = euclid::default::Rect<GrCoord>;
+pub type GrSize = euclid::default::Size2D<GrCoord>;
